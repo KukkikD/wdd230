9659b637447c8dce08187a4fc7a93bd82c62823d
KukkikD <adeezon9@gmail.com>
2024-04-24 19:47:36 -0700

    add my webpage

---
+++ week01/scripts/getDates.js
@@ -0,0 +1,5 @@
+// Copyright year
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+// Last modified date
+document.getElementById("lastModified").innerHTML = document.lastModified;
