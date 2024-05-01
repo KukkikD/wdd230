3ad67a7b0e14e9dc853d7cce34681170d5a99479
KukkikD <adeezon9@gmail.com>
2024-05-01 12:12:18 -0700

    added hamburger

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,12 @@ document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 
 // Last modified date
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+//Hamburger button
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menuLinks');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+})
