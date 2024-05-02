65dad8b6819d8d0450a7402c77a8cc58ef8d41f2
KukkikD <adeezon9@gmail.com>
2024-05-01 23:57:41 -0700

    added dark mode

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,6 +4,8 @@ document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 // Last modified date
 document.getElementById("lastModified").innerHTML = document.lastModified;
 
+
+
 //Hamburger button
 const hamburgerElement = document.querySelector('#myButton');
 const navElement = document.querySelector('.menuLinks');
@@ -12,3 +14,12 @@ hamburgerElement.addEventListener('click', () => {
     navElement.classList.toggle('open');
     hamburgerElement.classList.toggle('open');
 })
+
+//Dark mode//
+// Select the main element
+const main = document.querySelector('main');
+const myBtn = document.querySelector('#darkBtn');
+
+myBtn.addEventListener('click', () => {
+    main.classList.toggle('dark');
+});
