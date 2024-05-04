a9aacc51b10b783499b68fa26921df3c294df5a1
KukkikD <adeezon9@gmail.com>
2024-05-04 05:56:12 -0700

    add Chamber site

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,42 @@
+// Copyright year
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+//Lasmodify//
+document.getElementById('lastModified').textContent = new Date(document.lastModified).toLocaleDateString();
+
+
+
+//Hamburger button
+const hamburgerElement = document.querySelector('#menuButton');
+const navElement = document.querySelector('nav ul.menuLinks');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+})
+
+const joinElement = document.querySelector('a.join');
+
+// Add click event listener to every "read more..." link
+document.querySelectorAll('.read-more').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault(); // Prevent page from reloading when clicking the link
+
+        // If the "read more..." link is clicked, we will show/hide the additional information
+        const hiddenParagraph = this.nextElementSibling;
+        hiddenParagraph.classList.toggle('hidden');
+
+        // Remove the "read more" link after it's clicked
+        this.remove();
+
+    });
+});
+
+//Dark mode//
+// Select the main element
+//const main = document.querySelector('main');
+//const myBtn = document.querySelector('#darkBtn');
+
+//myBtn.addEventListener('click', () => {
+//    myBtn.classList.toggle('dark');
+//});
