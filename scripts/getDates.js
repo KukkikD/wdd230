b954da6857a658742bd336061e2a423cf114dc29
KukkikD <adeezon9@gmail.com>
2024-05-09 01:19:10 -0700

    added team activity

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,8 +1,8 @@
 // Copyright year
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 
-// Last modified date
-document.getElementById("lastModified").innerHTML = document.lastModified;
+//Lasmodify//
+document.getElementById('lastModified').textContent = new Date(document.lastModified).toLocaleDateString();
 
 
 
@@ -23,3 +23,23 @@ const myBtn = document.querySelector('#darkBtn');
 myBtn.addEventListener('click', () => {
     main.classList.toggle('dark');
 });
+
+//Number of visitor
+// Display element variable
+const visitsDisplay = document.querySelector(".visits");
+
+// Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+// If this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+// increment the number of visits by one.
+numVisits++;
+
+// store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numVisits-ls", numVisits);
